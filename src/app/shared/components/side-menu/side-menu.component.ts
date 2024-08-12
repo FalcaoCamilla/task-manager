8e63e3e6dd18831336d6d70d01481cacb4241211
Camilla Falcão <millafalcao.acad@gmail.com>
2024-08-11 22:21:22 -0300

    Feature | SideMenu - Criação de estrutura base para menu lateral

---
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBarsStaggered, faList, faTableColumns } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-side-menu',
+  standalone: true,
+  imports: [CommonModule, FontAwesomeModule, RouterModule],
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.scss'
+})
+export class SideMenuComponent {
+  expanded: boolean = true;
+  faList = faList;
+
+  protected menu = [
+    {
+      label: 'Dashboard',
+      routerLink: '/dashboard',
+      icon: faTableColumns
+    },
+    {
+      label: 'Tarefas',
+      routerLink: '/tasks',
+      icon: faBarsStaggered
+    }
+  ]
+
+  protected expand() {
+    this.expanded = !this.expanded
+  }
+}
