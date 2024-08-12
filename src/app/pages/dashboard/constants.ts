44896865463d58b2624f64158f394b5eef179186
Camilla Falcão <millafalcao.acad@gmail.com>
2024-08-11 22:36:42 -0300

    Refactor | Constants - Criação de constantes para formatação de dados do dashboard

---
+++ src/app/pages/dashboard/constants.ts
@@ -0,0 +1,13 @@
+import { faSpinner, faCheck, faTerminal } from '@fortawesome/free-solid-svg-icons';
+
+export const taskStatus = {
+  "total_pendentes": "to do",
+  "total_progresso": "doing",
+  "total_concluidas": "done"
+}
+
+export const taskIcons = {
+  "total_pendentes": faSpinner,
+  "total_progresso": faTerminal,
+  "total_concluidas": faCheck
+};
