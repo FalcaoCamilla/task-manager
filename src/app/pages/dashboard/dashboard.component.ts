97a09aa721a9a61f5982e18f82e34850d2d7e68e
Camilla Falcão <millafalcao.acad@gmail.com>
2024-08-11 22:36:16 -0300

    Feature | Dashboard - Criação de componente com estrutura inicial e utilização de cards e gráfico genéricos

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,68 @@
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { DashboardService } from '../../services/dashboard.service';
+import { cardData, DashboardData } from '../../shared/models';
+import { ToastrService } from 'ngx-toastr';
+import { taskIcons, taskStatus } from './constants';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { LineChartComponent } from '../../shared/components/line-chart/line-chart.component';
+import { CardComponent } from "../../shared/components/card/card.component";
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CardModule, FontAwesomeModule, LineChartComponent, CardComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent implements OnInit {
+  private _dashboardservice = inject(DashboardService);
+  private _toastr = inject(ToastrService);
+
+  protected dashboardData: WritableSignal<DashboardData> = signal({
+    chartData: [], 
+    cardData: {
+      total_concluidas: 0,
+      total_pendentes: 0,
+      total_progresso: 0
+    }
+  });
+
+  protected showLineChart: WritableSignal<boolean> = signal(false);
+
+  get cardEntries() {
+    return Object.entries(this.dashboardData().cardData as cardData)
+  }
+
+  ngOnInit(): void {
+    this._dashboardservice.getDashboardData().subscribe({
+      next: (data) => {
+        this.dashboardData.set(data);
+        this.showLineChart.set(true)
+      },
+      error: () => this._toastr.error('Falha na requisição')
+    })
+  }
+
+  // private filterTasksByDate(finishDate: Date | null | undefined, filter: 'last15' | 'last30' | 'today', today: Date): boolean {
+  //   if (!finishDate) {
+  //     return false;
+  //   }
+  
+  //   const finishDateObject = new Date(finishDate);
+  
+  //   switch (filter) {
+  //     case 'last15':
+  //       return finishDateObject >= new Date(today.setDate(today.getDate() - 15));
+  //     case 'last30':
+  //       return finishDateObject >= new Date(today.setDate(today.getDate() - 30));
+  //     case 'today':
+  //       return finishDateObject.getFullYear() === today.getFullYear() &&
+  //              finishDateObject.getMonth() === today.getMonth() &&
+  //              finishDateObject.getDate() === today.getDate();
+  //   }
+  
+  //   return false;
+  // }
+}
