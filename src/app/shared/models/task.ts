58686227f78a93e3a4ae1b350d132c42ed1d3307
Camilla Falcão <millafalcao.acad@gmail.com>
2024-08-11 22:13:04 -0300

    Refactor | TaskModel - Criação de model para dados das tarefas

---
+++ src/app/shared/models/task.ts
@@ -0,0 +1,18 @@
+import { User } from "./user"
+
+export type Task = {
+  id: number, 
+  name: string, 
+  description: string, 
+  status: status, 
+  priority: priority, 
+  project: string, 
+  responsible: User, 
+  deadline: Date, 
+  file?: File, 
+  finish_date?: Date | null
+}
+
+type status = 'concluido' | 'em_progresso' | 'pendente'
+
+type priority = 'alta' | 'media' | 'baixa'
