c14f903c84500af17945a5443f413b8e5e0b1867
Camilla Falcão <millafalcao.acad@gmail.com>
2024-08-11 22:20:35 -0300

    Refactor | Constants - Criação de const para formatação de dados

---
+++ src/app/shared/models/constants.ts
@@ -0,0 +1,11 @@
+export const status = {
+  concluido: 'Concluído',
+  em_progresso: 'Em progresso',
+  pendente: 'Pendente'
+}
+
+export const priority = {
+  alta: 'Alta',
+  media: 'Média',
+  baixa: 'Baixa'
+}
