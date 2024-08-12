e88eb20ec91e81d1795ed78905b86f9420a51f43
Camilla Falcão <millafalcao.acad@gmail.com>
2024-08-11 22:31:19 -0300

    Feature | InMemoryService - Criação de service para simulação de API

---
+++ src/app/services/in-memory.service.ts
@@ -0,0 +1,97 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Task, User } from '../shared/models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryService implements InMemoryDbService {
+  constructor() { }
+
+  createDb() {
+    const users: User[] = [
+      {
+        id: 1,
+        name: 'Camilla Falcão',
+        email: 'camilla.falcao@email.com'
+      },
+      {
+        id: 2,
+        name: 'Euclides Neto',
+        email: 'euclides.neto@email.com'
+      },
+      {
+        id: 3,
+        name: 'Hugo Falcão',
+        email: 'hugo.falcao@email.com'
+      },
+    ]
+    const tasks: Task[] = [
+      { 
+        id: 1, 
+        name: 'Documentação', 
+        description: 'Documentar o projeto Task Manager', 
+        status: 'concluido', 
+        priority: 'media', 
+        project: 'Task Manager', 
+        responsible: users[0], 
+        deadline: new Date(2024, 8, 12, 12, 0, 0),
+        finish_date: new Date(2024, 8, 10, 12, 0, 0) 
+      },
+      { 
+        id: 2, 
+        name: 'Construção BD', 
+        description: 'Criação do Banco de Dados para projeto', 
+        status: 'concluido', 
+        priority: 'media', 
+        project: 'Task Manager', 
+        responsible: users[1], 
+        deadline: new Date(2024, 8, 13, 12, 0, 0), 
+        finish_date: new Date(2024, 8, 11, 12, 0, 0) 
+      },
+      { 
+        id: 3, 
+        name: 'Elaboração de protótipo', 
+        description: 'Elaboração de protótipo para etapa criativa do projeto', 
+        status: 'concluido', 
+        priority: 'alta', 
+        project: 'Task Manager', 
+        responsible: users[2], 
+        deadline: new Date(2024, 8, 12, 12, 0, 0),
+        finish_date: new Date(2024, 8, 11, 12, 0, 0) 
+      },
+      { 
+        id: 4, 
+        name: 'Deploy da aplicação', 
+        description: 'Fazer deploy do projeto Task Manager', 
+        status: 'concluido', 
+        priority: 'baixa', 
+        project: 'Task Manager', 
+        responsible: users[0], 
+        deadline: new Date(2024, 8, 12, 12, 0, 0),
+        finish_date: new Date(2024, 8, 12, 12, 0, 0) 
+      }, 
+      { 
+        id: 5, 
+        name: 'Prova Técnica', 
+        description: 'Elaboração do gestor de tarefas', 
+        status: 'em_progresso', 
+        priority: 'alta', 
+        project: 'Task Manager', 
+        responsible: users[0], 
+        deadline: new Date(2024, 8, 13, 12, 0, 0)
+      },
+      { 
+        id: 6, 
+        name: 'Pesquisa Angular', 
+        description: 'Estudar novos conceitos da versão LTS do Angular', 
+        status: 'pendente', 
+        priority: 'baixa', 
+        project: '', 
+        responsible: users[1], 
+        deadline: new Date(2024, 8, 10, 12, 0, 0)
+      },
+    ]
+    return { users, tasks}
+  }
+}
