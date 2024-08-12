2555d853c99d9abd3bd7966618e02d1750cb95ca
Camilla Falcão <millafalcao.acad@gmail.com>
2024-08-11 22:25:40 -0300

    Feature | Card - Criação de componente genérico para card

---
+++ src/app/shared/components/card/card.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { taskStatus, taskIcons } from '../../../pages/dashboard/constants';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [CardModule, FontAwesomeModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent implements OnInit {
+  @Input() cardDataEntrie: any;
+
+  protected taskStatus: {[key: string]: string} = taskStatus;
+  protected taskIcons: {[key: string]: IconDefinition} = taskIcons;
+
+  ngOnInit(): void {
+    console.log(this.cardDataEntrie)
+  }
+}
