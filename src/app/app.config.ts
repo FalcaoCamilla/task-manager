8f9cf0b5c768c87b68a94f6a13439b8867e8d059
Camilla Falcão <millafalcao.acad@gmail.com>
2024-08-11 22:38:31 -0300

    Config | Core - Estrutura inicial de arquivos config e route

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations'
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'
+import { InMemoryService } from './services/in-memory.service';
+import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
 
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideHttpClient(),
+    provideAnimations(),
+    provideToastr(), 
+    importProvidersFrom(
+      HttpClientInMemoryWebApiModule.forRoot(InMemoryService, {dataEncapsulation: false}),
+    )
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+export const routes: Routes = [
+  // {path: '', component: LoginComponent, canActivate: [authGuard]},
+  {path: '', component: DashboardComponent},
+  {path: '**', redirectTo: ''}
+];
