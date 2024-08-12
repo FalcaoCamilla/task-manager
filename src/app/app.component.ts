a0e20aa8ffc4226f77135ab458f79713cdf56c2e
Camilla Falcão <millafalcao.acad@gmail.com>
2024-08-11 22:39:20 -0300

    Refactor | App - Estruturação inicial no template principal da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenuComponent } from "./shared/components/side-menu/side-menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'task-dashboard';
+  title = 'Task Manager';
 }
