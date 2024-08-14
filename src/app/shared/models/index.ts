a6fbec1b850a1971f8a20a4d4ae33d380b363b77
Camilla Falcão <millafalcao.acad@gmail.com>
2024-08-13 23:19:50 -0300

    Refactor | SelectDataModel - Novo model para opções de dropdown

--- src/app/shared/models/index.ts
+++ src/app/shared/models/index.ts
@@ -2,3 +2,4 @@ export * from './constants';
 export * from './task';
 export * from './user';
 export * from './dashboard-data';
+export * from './selectData';
---
+++ src/app/shared/models/selectData.ts
@@ -0,0 +1,4 @@
+export type SelectData = {
+  id: number,
+  text: string
+}
