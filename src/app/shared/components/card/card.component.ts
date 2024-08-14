744a2100e67529bfd51f850c76e67851a98edd1f
Camilla Falcão <millafalcao.acad@gmail.com>
2024-08-14 01:48:54 -0300

    Refactor | Card - Novos dados de tarefas atrasadas *

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, signal, WritableSignal } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -12,13 +12,10 @@ import { taskStatus, taskIcons } from '../../../pages/dashboard/constants';
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
-export class CardComponent implements OnInit {
-  @Input() cardDataEntrie: any;
+export class CardComponent {
+  @Input() cardDataEntrie: [string, number] = ['', 0];
+  @Input() overdueTasks: number = 0;
 
   protected taskStatus: {[key: string]: string} = taskStatus;
   protected taskIcons: {[key: string]: IconDefinition} = taskIcons;
-
-  ngOnInit(): void {
-    console.log(this.cardDataEntrie)
-  }
 }
