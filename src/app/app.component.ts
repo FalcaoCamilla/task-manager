6740cf7d8e175c2ea7f73187276efe0047b7f8a1
Camilla Falcão <millafalcao.acad@gmail.com>
2024-08-13 23:14:03 -0300

    Refactor | App - Reestruturação do template para novos componentes + inclusão url da imagem de perfil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SearchComponent } from './shared/components/search/search.component';
 import { SideMenuComponent } from "./shared/components/side-menu/side-menu.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SideMenuComponent],
+  imports: [RouterOutlet, SearchComponent, SideMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Task Manager';
+  urlImg: string = 'https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg'
 }
