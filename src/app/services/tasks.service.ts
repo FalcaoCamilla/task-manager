b982274210b88a0f23da6a75ca581d9b2e3749ca
Camilla Falcão <millafalcao.acad@gmail.com>
2024-08-14 04:50:52 -0300

    Feature | TasksService - Implementado método para buscar dados de tarefas

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task } from '../shared/models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaksService {
+  protected http = inject(HttpClient);
 
-  constructor() { }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>('/api/tasks')
+  }
 }
