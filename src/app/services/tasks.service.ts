6049d16374bb150291a490407d71d805abcd3d50
Camilla Falcão <millafalcao.acad@gmail.com>
2024-08-14 11:28:54 -0300

    Feature | TaskService - Novo método para filtragem de projetos (não cadastrados individualmente) dentro das tarefas

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Task } from '../shared/models';
+import { map, Observable } from 'rxjs';
+import { SelectData, Task } from '../shared/models';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +9,20 @@ import { Task } from '../shared/models';
 export class TaksService {
   protected http = inject(HttpClient);
 
-
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>('/api/tasks')
+  }
+
+  getProjects(): Observable<SelectData[]> {
+    return this.http.get<Task[]>('/api/tasks')
+      .pipe(
+        map(tasks => {
+          const uniqueProjects = new Set(tasks.map(task => task.project));
+          return [...uniqueProjects].map((projectName, index) => ({
+            id: index + 1,
+            text: projectName
+          }));
+        })
+      );
   }
 }
