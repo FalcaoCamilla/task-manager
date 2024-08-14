63f02f55a6a0e9865ff22bba34e08bbcf2903a01
Camilla Falcão <millafalcao.acad@gmail.com>
2024-08-14 04:51:10 -0300

    Refactor | Routes - Criação de rota para component task

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
 
 export const routes: Routes = [
   // {path: '', component: LoginComponent, canActivate: [authGuard]},
-  {path: '', component: DashboardComponent},
-  {path: '**', redirectTo: ''}
+  {path: '', pathMatch: 'full', redirectTo: 'dashboard'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'tasks', component: TasksComponent},
 ];
