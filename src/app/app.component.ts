3926e9ecb41792f4378c3699d37fe90e9b2df564
Camilla Falcão <millafalcao.acad@gmail.com>
2024-08-15 18:04:56 -0300

    Refactor | App - Refatoração para template exibido através do novo BaseComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,5 +12,4 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Task Manager';
-  urlImg: string = 'https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg'
 }
