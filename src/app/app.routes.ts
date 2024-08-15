945e12d5bc4fcf7945bfb0b3d53edcaeffdeb7fa
Camilla Falcão <millafalcao.acad@gmail.com>
2024-08-15 18:05:59 -0300

    Refactor | Routes - Novo esquema de rotas para implementação de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,27 @@ import { Routes } from '@angular/router';
 
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { TasksComponent } from './pages/tasks/tasks.component';
+import { authGuard } from './guards/auth.guard';
+import { LoginComponent } from './pages/login/login.component';
+import { BaseComponent } from './pages/base/base.component';
 
 export const routes: Routes = [
-  // {path: '', component: LoginComponent, canActivate: [authGuard]},
-  {path: '', pathMatch: 'full', redirectTo: 'dashboard'},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'tasks', component: TasksComponent},
+  {
+    path: '',
+    component: BaseComponent,
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'dashboard',
+        //carregar o componente somente ao acessar a rota
+        loadComponent: () => import('../app/pages/dashboard/dashboard.component').then(m => m.DashboardComponent)
+      },
+      {
+        path: 'tasks',
+        loadComponent: () => import('../app/pages/tasks/tasks.component').then(m => m.TasksComponent)
+      },
+    ]
+  },
+  {path: 'login', component: LoginComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'login'},
 ];
