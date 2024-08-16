08e9e5f6b37cdb643a0d2fe390f8aab071725487
Camilla Falcão <millafalcao.acad@gmail.com>
2024-08-15 23:52:30 -0300

    Refactor | Card - Refatoração para utilização de inputSignal

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, signal, WritableSignal } from '@angular/core';
+import { Component, input, Input, OnInit, signal, WritableSignal } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -13,8 +13,8 @@ import { taskStatus, taskIcons } from '../../../pages/dashboard/constants';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-  @Input() cardDataEntrie: [string, number] = ['', 0];
-  @Input() overdueTasks: number = 0;
+  cardDataEntrie = input<[string, number]>(['', 0]);
+  overdueTasks = input<number>(0);
 
   protected taskStatus: {[key: string]: string} = taskStatus;
   protected taskIcons: {[key: string]: IconDefinition} = taskIcons;
