6ffee5bc3c161091f62af32ecbca5e959a96cc45
Camilla Falcão <millafalcao.acad@gmail.com>
2024-08-16 22:40:50 -0300

    Refactor | LoginService - Integração de métodos de login, singup e logout com fake api

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,7 +1,10 @@
 import { isPlatformBrowser } from '@angular/common';
-import { inject, Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { Observable, of, map, tap, catchError } from 'rxjs';
+import { User } from '../shared/models';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,32 +12,33 @@ import { Observable, of } from 'rxjs';
 export class LoginService {
   private platformId = inject(PLATFORM_ID);
   private _router = inject(Router);
+  private _http = inject(HttpClient);
+  private _userService = inject(UserService);
   //PLATFORM_ID identifica a plataforma em que o aplicativo está sendo executado (navegador ou servidor). Esse ID é armazenado em platformId para uso posterior.
 
-  public login(formLogin: {username: string, password: string}) {
-    const storedLogin = localStorage.getItem("login");
-    const login = storedLogin ? JSON.parse(storedLogin) : null;
-
-    if(!login || !(login.username === formLogin.username && login.password === formLogin.password)) {
-      return of(false);
-    }
-
-    //Verifica se o código está sendo executado no navegador. Se for, armazena um token de autenticação no localStorage.
-    if(isPlatformBrowser(this.platformId)) {
-      localStorage.setItem('token', 'registered')
-    }
-    return of(true);
+  public login(formLogin: User): Observable<boolean> {
+    return this._userService.getUsers().pipe(
+      map(users => users.find(user => user.email === formLogin.email && user.password === formLogin.password) || null), //o uso do || null indica que, caso não encontrado usuário, ao invés de retornar undefined, deve retornar null
+      tap(user => { //recebe o valor emitido pelo map como argumento
+        if (user && isPlatformBrowser(this.platformId)) {
+          localStorage.setItem('token', 'registered');
+          this._userService.setLoggedInUser(user);
+        }
+      }),
+      map(user => !!user) // Converte o usuário encontrado em um booleano (true se encontrado, false caso contrário)
+    );
   }
 
-  public signUp(formLogin: {username: string, email: string, password: string}): Observable<boolean> {
-    localStorage.setItem("login", JSON.stringify(formLogin));
-    const storedLogin = localStorage.getItem("login");
-  
-    return storedLogin ? of(true) : of(false);
+  public signUp(formLogin: User): Observable<boolean> {
+    return this._userService.setUser(formLogin).pipe(
+      map(() => true),
+      catchError(() => of(false))
+    );
   }
 
   public logout(): void {
     localStorage.removeItem('token');
+    this._userService.setLoggedInUser(null);
     this._router.navigateByUrl('/login')
   }
 
